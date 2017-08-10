dd1c46126cd42103556701b0084a2a9cabb32ec0
Farooq Khan <farooqahmadkhan003@gmail.com>
2017-08-10 10:48:05 +0500

    final upload

--- app/src/main/java/com/androidtechpoint/customview/MainActivity.java
+++ app/src/main/java/com/androidtechpoint/customview/MainActivity.java
@@ -3,14 +3,17 @@
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+    public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+    public void newActivity(View v){
         startActivity(new Intent(this , CardActivity.class));
     }
 }
